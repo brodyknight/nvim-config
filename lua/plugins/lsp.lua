965dba6ef1de7030c32f2498a8e24303304e25e0
Brody Knight <git@brodyk.com>
2025-02-11 14:26:00 +0000

    Add icons for cmp suggestions

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -121,6 +121,44 @@ return {
             select = true,
           }),
         }),
+        formatting = {
+          format = function(entry, vim_item)
+            -- Icons for different completion sources
+            local kind_icons = {
+              Text = "󰉿",
+              Method = "󰆧",
+              Function = "󰊕",
+              Constructor = "",
+              Field = "󰜢",
+              Variable = "󰀫",
+              Class = "󰠱",
+              Interface = "",
+              Module = "",
+              Property = "󰜢",
+              Unit = "󰑭",
+              Value = "󰎠",
+              Enum = "",
+              Keyword = "󰌋",
+              Snippet = "",
+              Color = "󰏘",
+              File = "󰈙",
+              Reference = "󰈇",
+              Folder = "󰉋",
+              EnumMember = "",
+              Constant = "󰏿",
+              Struct = "󰙅",
+              Event = "",
+              Operator = "󰆕",
+              TypeParameter = "",
+              Copilot = "",
+            }
+            -- Add icon to vim_item
+            vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
+
+            return vim_item
+          end
+
+        },
         sources = cmp.config.sources({
           { name = "copilot" },
           { name = "nvim_lsp" },
