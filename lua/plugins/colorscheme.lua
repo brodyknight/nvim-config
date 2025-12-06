ebdc5c4aebdf08d5f016151f65f5f280b72eca53
Brody Knight <git@brodyk.com>
2025-12-06 16:16:46 +0000

    Update colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,21 @@
-return {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("gruvbox")
-    end,
+local active = "kanagawa-dragon"
+
+local themes = {
+  { "rebelot/kanagawa.nvim", name = "kanagawa" },
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "folke/tokyonight.nvim", name = "tokyonight" },
+  { "rose-pine/neovim", name = "rose-pine" },
 }
+
+for _, theme in ipairs(themes) do
+  if theme.name == active then
+    theme.priority = 1000
+    theme.config = function()
+      vim.cmd.colorscheme(active)
+    end
+  else
+    theme.lazy = true
+  end
+end
+
+return themes
