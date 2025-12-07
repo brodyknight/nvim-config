94819c9d0ab0649209444ee2c7d9453a48158b52
Brody Knight <git@brodyk.com>
2025-12-07 11:27:16 +0000

    Add treesitter context plugin

---
+++ lua/plugins/treesitter-context.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-treesitter/nvim-treesitter-context",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  opts = {
+    enable = true,
+    line_numbers = true,
+    mode = 'cursor',
+  }
+}
